367b7bacf611bb0f7bcae4d4fbf7f1e2c6654982
Daniel Kazemian <achilles.7733@gmail.com>
2026-06-14 23:32:41 +0300

    Add formula to the main

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+"""
+            Meaurement that I use in this program:
+                Distance meaurement: m
+                Mass measurement: Kg
+                Speed meaurement: m/s
+                Acceleration meaurement: m/s^2
+                Eneregy meaurement: J
+                Force meaurement: N
+
+            classes and there duties
+                Plane:
+                    State of aircraft
+
+                AutoPilot:
+                    Decisions
+
+                FlightCalculator:
+                    Physics calculations. Ex -> distance = calculator.max_glide_distance(plane)
+
+                MissionPlanner:
+                    Goals
+"""
+
 from plane import Plane
 from autopilot import AutoPilot
 from config import GRAVITY, TIME_STEP
@@ -28,10 +51,15 @@ def main():
             #f"Vertical speed is: {plane.vertical_speed:.2f}m/s"
         #)
 
+        KE: float = 1/2 * plane.mass * plane.vertical_speed**2
+        PE: float = plane.mass * abs(GRAVITY) * plane.altitude
+        E: float = KE + PE
+        distance: float = plane.altitude * (plane.calculate_lift() / plane.drag)
+
         print(
             f"Time: {time_elapsed:.1f}s | "
             f"Pitch Angle: {plane.pitch_angle:.2f}° | "
-            f"Initial Pitch: {plane.pitch_angle} | "
+            #f"Initial Pitch: {plane.pitch_angle} | "
             f"Flight Path Angle: {plane.flight_path_angle():.2f}° | "
             f"AoA: {plane.aoa:.2f}° | "
             f"Altitude: {plane.altitude:.2f}m | "
@@ -39,7 +67,12 @@ def main():
             f"Vertical Speed: {plane.vertical_speed:.2f}m/s | "
             f"Lift: {plane.calculate_lift():.2f}N | "
             f"Drag: {plane.drag:.2f}N | "
+            f"KE: {KE:,.2f}J | "
+            f"PE: {PE:,.2f}J | "
+            f"E: {E:,.2f}J | "
+            f"estimated_distance: {distance:,.2f}m | " # for now My lift and drag are simplified simulator values and that's why I said estimated until i use correct form of drag, lift,... formula
             )
+
         
     print("The plane has reached the ground.")
 
