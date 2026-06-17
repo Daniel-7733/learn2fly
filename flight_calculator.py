1634a88c061a58df8100f506ba866829aa3be964
Daniel Kazemian <achilles.7733@gmail.com>
2026-06-17 23:52:50 +0300

    Add calculations formula for plane

---
+++ flight_calculator.py
@@ -0,0 +1,69 @@
+class FlightCalculator:
+    """
+    shared calculations
+
+    Measurement that I use in this program:
+        Distance Measurement: m
+        Mass Measurement: Kg
+        Speed Measurement: m/s
+        Acceleration Measurement: m/s^2
+        Eneregy Measurement: J
+        Force Measurement: N
+
+    """
+
+    @staticmethod
+    def kinetic_energy(mass: float, speed: float) -> float:
+        """This function calulate kinetic energy: KE = 1/2 mv^2"""
+        return 0.5 * mass * speed ** 2
+
+    @staticmethod
+    def potential_energy(mass: float, gravity: float, altitude: float) -> float:
+        """This function calculate potential energy: PE = mgh"""
+        return mass * abs(gravity) * altitude
+
+    @staticmethod
+    def total_energy(mass: float, gravity: float, altitude: float, speed: float) -> float:
+        """This function calculate the total energy: E = mgh + 1/2 mv^2"""
+        return (
+            FlightCalculator.kinetic_energy(mass, speed)
+            + FlightCalculator.potential_energy(mass, gravity, altitude)
+        )
+
+    @staticmethod
+    def specific_energy(gravity: float, altitude: float, speed: float) -> float:
+        """
+        This function will calculate the specific energy by removing the mass from right side of formula.
+        E = mgh + 1/2 mv^2 -> at the end we receive Energy (Like 1000 J)
+        E/m = gh + v^2 / 2 -> at the end we receive Energy per kilogram (Like 500 J/KG)
+        """
+        return abs(gravity) * altitude + 0.5 * speed ** 2
+
+    @staticmethod
+    def estimated_glide_distance(altitude: float, lift: float, drag: float) -> float:
+        """
+        With this formula we will get the gliding distance that we can travel.
+        formula: distance = altitude * (lift / drag)
+        Note: Right now, it is estimated answer because our formula for drag, lift is not complete yet
+        """
+        if drag <= 0:
+            return 0.0
+        return altitude * (lift / drag)
+    
+    @staticmethod
+    def total_speed(horizontal_speed: float, vertical_speed: float) -> float:
+        """
+        This function calculate the both speeds and return total speed. How did I find this formula:
+
+        1. c^2 = a^2 + b^2        ---OR--- (Vx^2 + Vy^2 = V^2)
+        2.   c = root(a^2 + b^2)
+        3.   c = (a^2 + b^2)^1/2  ---OR--- c = (a^2 + b^2)^0.5
+
+        So, this is the formula -> (Vx^2 + Vy^2)^0.5
+        """
+        return (horizontal_speed**2 + vertical_speed**2) ** 0.5
+
+    @staticmethod
+    def energy_rate(previous_energy: float, current_energy: float, time_step: float) -> float:
+        """Energy rate is Energy rate = de/dt"""
+        return (current_energy - previous_energy) / time_step
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 """
-            Meaurement that I use in this program:
-                Distance meaurement: m
-                Mass measurement: Kg
-                Speed meaurement: m/s
-                Acceleration meaurement: m/s^2
-                Eneregy meaurement: J
-                Force meaurement: N
+            Measurement that I use in this program:
+                Distance Measurement: m
+                Mass Measurement: Kg
+                Speed Measurement: m/s
+                Acceleration Measurement: m/s^2
+                Eneregy Measurement: J
+                Force Measurement: N
 
             classes and there duties
                 Plane:
@@ -23,6 +23,7 @@
 
 from plane import Plane
 from autopilot import AutoPilot
+from flight_calculator import FlightCalculator
 from config import GRAVITY, TIME_STEP
 
 
@@ -32,49 +33,62 @@ def main():
     plane: Plane = Plane(altitude=1200, horizontal_speed=85, pitch_angle=10, mass=20.0)
     autopilot: AutoPilot = AutoPilot()
 
-    while plane.altitude > 0:
+    current_energy: float = FlightCalculator.total_energy(
+        plane.mass, GRAVITY, plane.altitude,
+        FlightCalculator.total_speed(plane.horizontal_speed, plane.vertical_speed)
+    )
 
+    while plane.altitude > 0:
         plane.aoa = plane.calculate_aoa()
         autopilot.update(plane)
 
+        previous_energy: float = current_energy
+
         plane.update_physics(GRAVITY, TIME_STEP)
+
+        total_speed: float = FlightCalculator.total_speed(plane.horizontal_speed, plane.vertical_speed)
+        current_energy: float = FlightCalculator.total_energy(plane.mass, GRAVITY, plane.altitude, total_speed)
+        energy_rate: float = FlightCalculator.energy_rate(previous_energy, current_energy, TIME_STEP)
+
         time_elapsed += TIME_STEP
+
         
         #if time_elapsed == 2:
             #plane.pitch_up(30)
-
-        #print(
-            #f"Time: {time_elapsed:.1f}s | "
-            #f"Altitude: {plane.altitude:.2f}m | "
-            #f"horizontal Speed: {plane.horizontal_speed:.2f}m/s | "
-            #f"Angle: {plane.angle:.2f}° | "
-            #f"Vertical speed is: {plane.vertical_speed:.2f}m/s"
-        #)
-
-        KE: float = 1/2 * plane.mass * plane.vertical_speed**2
-        PE: float = plane.mass * abs(GRAVITY) * plane.altitude
-        E: float = KE + PE
-        distance: float = plane.altitude * (plane.calculate_lift() / plane.drag)
+        
+        ke: float = FlightCalculator.kinetic_energy(plane.mass, total_speed)
+        pe: float = FlightCalculator.potential_energy(plane.mass, GRAVITY, plane.altitude)
+        energy: float = FlightCalculator.total_energy(plane.mass, GRAVITY, plane.altitude, total_speed)
+        specific_energy: float = FlightCalculator.specific_energy(GRAVITY, plane.altitude, total_speed)
+        estimated_distance: float = FlightCalculator.estimated_glide_distance(plane.altitude, plane.calculate_lift(), plane.drag)
 
         print(
             f"Time: {time_elapsed:.1f}s | "
+
             f"Pitch Angle: {plane.pitch_angle:.2f}° | "
             #f"Initial Pitch: {plane.pitch_angle} | "
             f"Flight Path Angle: {plane.flight_path_angle():.2f}° | "
             f"AoA: {plane.aoa:.2f}° | "
+
             f"Altitude: {plane.altitude:.2f}m | "
+            f"estimated_distance: {estimated_distance:,.2f}m | "
+
             f"horizontal Speed: {plane.horizontal_speed:.2f}m/s | "
             f"Vertical Speed: {plane.vertical_speed:.2f}m/s | "
+            f"Total Speed: {total_speed:.2f} m/s | "
+
             f"Lift: {plane.calculate_lift():.2f}N | "
             f"Drag: {plane.drag:.2f}N | "
-            f"KE: {KE:,.2f}J | "
-            f"PE: {PE:,.2f}J | "
-            f"E: {E:,.2f}J | "
-            f"estimated_distance: {distance:,.2f}m | " # for now My lift and drag are simplified simulator values and that's why I said estimated until i use correct form of drag, lift,... formula
+
+            f"KE: {ke:,.2f}J | "
+            f"PE: {pe:,.2f}J | "
+            f"E: {energy:,.2f}J | "
+            f"Specific Energy: {specific_energy:,.2f}J/KG | "
+            f"Energy Rate: {energy_rate:,.2f}J | "
             )
 
-        
     print("The plane has reached the ground.")
+
 
 
 if __name__ == "__main__":
